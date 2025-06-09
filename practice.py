f8e86bf4fd167ba4e54df47f5ae655aa8804e158
AmayaTibana <amayatibana@gmail.com>
2025-06-09 14:15:34 -0400

    Day 51

--- practice.py
+++ practice.py
@@ -578,3 +578,72 @@ while True:
    
 prettyPrint()
 # This is the end of the practice.py file
+
+
+
+listOfShame = [] 
+
+menu = input("Add or Remove?") 
+
+if(menu.strip().lower()[0]=="a"): 
+    name = input("What is your name? ")
+    age = input("What is your age? ")
+    pref = input("What is your computer platform? ")
+    row = [name, age, pref] 
+    listOfShame.append(row) 
+else: 
+    name = input("What is the name of the record to delete?") 
+
+    if name in listOfShame: 
+      for row in name:
+        listOfShame.remove(row) # remove the whole row if name is in it
+
+print(listOfShame)
+
+
+# This is the end of the practice.py file
+
+
+import random, os, time
+bingo = []
+def ran():
+  number = random.randint(1,90)
+  return number
+def prettyPrint():
+  for row in bingo:
+    for item in row:
+      print(item, end="\t|\t")
+    print()
+def createCard():
+  global bingo
+  numbers = []
+  for i in range(8):
+    num = ran()
+    while num in numbers:
+      num = ran()
+    numbers.append(ran())
+
+  numbers.sort()
+
+  bingo = [ [ numbers[0], numbers[1], numbers[2]],
+            [ numbers[3], "BG", numbers[4] ],
+            [ numbers [5], numbers[6], numbers[7]]
+          ]
+createCard()
+while True:
+  prettyPrint()
+  num = int(input("Next Number: "))
+  for row in range(3):
+    for item in range(3):
+      if bingo[row][item] == num:
+        bingo[row][item] = "X"
+  exes = 0
+  for row in bingo:
+    for item in row:
+      if item=="X":
+        exes+=1
+  if exes == 8:
+    print("You have won")
+    break
+  time.sleep(1)
+  os.system("clear")
