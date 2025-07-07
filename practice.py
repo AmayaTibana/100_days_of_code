af47390140cda9d00c36bc645e00d7f9eba56c7b
AmayaTibana <amayatibana@gmail.com>
2025-07-07 13:47:16 -0400

    si

--- practice.py
+++ practice.py
@@ -1,22 +1,4 @@
-clue = {}
-
-def PrettyPrint():
-  print()
-  for keys, values in clue.items():
-    print(keys, end=": ")
-    for subkey, subvalue in values.items():
-      print (subkey, subvalue, end = " | ")
-    print()
-
-while True:
-  name = input("Enter a name: ")
-  location = input("Enter a location: ")
-  weapon = input("Enter a weapon: ")
-  clue[name] = {"location":location, "weapon":weapon}
-
-  PrettyPrint()
-
-  import random, os, time
+import random, os, time
 
 mokedex = {}
 
