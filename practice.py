1f27e59e126f95897658d77cc40a8e1161a66dda
AmayaTibana <amayatibana@gmail.com>
2025-07-09 10:35:17 -0400

    To Do List

--- practice.py
+++ practice.py
@@ -1,54 +1,79 @@
+# | Name | Date | Priority
+import os, time
 
+todo = []
+f = open("todo.csv", "r")
+todo = eval(f.read())
+f.close()
 
-import random, os, time
-
-import os, time, random
 def add():
-  os.system("clear")
-  idea = input("Idea > ")
-  f = open("my.ideas", "a+")
-  f.write(f"{idea}\n")
-  f.close()
   time.sleep(1)
   os.system("clear")
-def show():
+  name = input("Name > ")
+  date = input("Due Date > ")
+  priority = input("Priority > ").capitalize()
+  row = [name, date, priority]
+  todo.append(row)
+  print("Added")
+def view():
+  time.sleep(1)
   os.system("clear")
-  f = open("my.ideas", "r")
-  ideas = f.read().split("\n")
-  f.close()
-  ideas.remove("")
-  idea = random.choice(ideas)
-  print(idea)
-  time.sleep(2)
+  options = input("1: All\n2: By Priority\n> ")
+  if options=="1":
+    for row in todo:
+      for item in row:
+        print(item, end=" | ")
+      print()
+    print()
+  else:
+    priority = input("What priority? > ").capitalize()
+    for row in todo:
+      if priority in row:
+        for item in row:
+          print(item, end=" | ")
+        print()
+    print()
+  time.sleep(1)
+def edit():
+  time.sleep(1)
   os.system("clear")
+  find = input("Name of todo to edit > ")
+  found = False
+  for row in todo:
+    if find in row:
+      found = True
+  if not found:
+    print("Couldn't find that")
+    return
+  for row in todo:
+    if find in row:
+      todo.remove(row)
+  name = input("Name > ")
+  date = input("Due Date > ")
+  priority = input("Priority > ").capitalize()
+  row = [name, date, priority]
+  todo.append(row)
+  print("Added")
+def remove():
+  time.sleep(1)
+  os.system("clear")
+  find = input("Name of todo to remove > ")
+  for row in todo:
+    if find in row:
+      todo.remove(row)
 while True:
-  menu = input("1: Add idea\n2: Show a random idea\n> ")
+  menu = input("1: Add\n2: View\n3: Edit\n4: Remove\n> ")
   if menu == "1":
     add()
+  elif menu == "2":
+    view()
+  elif menu == "3":
+    edit()
   else:
-    show()
-
-myEvents = {}
-
-def prettyPrint ():
-    for row in myEvents:
-      print (f"{row[0] :15^}. {row[1] : 15^}")
-    print()
-
-while True:
-    menu = input("1. Add Event\n2 Delete Event\n")
-    if menu == "1":
-       event = input("Enter Event: ")
-       date = input("Enter Date: ")
-       row = [date, event]
-       myEvents.append(row)
-       prettyPrint()
-    else:
-       criteria = input ("Enter event you want to delete")
-       for row in myEvents:
-           if criteria in row:
-               myEvents.remove(row)
-
-    f = open("events.txt","w")
-    f.write(str(myEvents))
-    f.close()
+    remove()
+    
+  time.sleep(1)
+  os.system("clear")
+  f = open("todo.csv", "w")
+  f.write(str(todo))
+  f.close()
