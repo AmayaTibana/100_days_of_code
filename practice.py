ec035d4685387739d9880611487930a0bd1241f9
AmayaTibana <amayatibana@gmail.com>
2025-07-09 15:28:05 -0400

    Video game inventory day 53

--- practice.py
+++ practice.py
@@ -1,57 +1,66 @@
+
 import os, time, sys
 
-pizza = []
-
+inventory = []
 try:
-  f = open("pizza.txt", "r")
-  pizza = eval(f.read())
+  f = open("inventory.txt", "r")
+  inventory = eval(f.read())
   f.close()
 except:
-  print("ERROR: No existing pizza No file found")
+    print("No inventory file found. Creating new inventory file.")
 
-def viewPizza():
-  h1 = "Name"
-  h2 = "Topping"
-  h3 = "Size"
-  h4 = "Quantity"
-  h5 = "Total"
-  print(f"{h1:^10}{h2:^10}{h3:^10}{h4:^10}{h5:^10}")
-  for row in pizza:
-    print(f"{row[0]:^10}{row[1]:^10}{row[2]:^10}{row[3]:^10}{row[4]:^10}")
-    time.sleep(2)
-
-def addPizza():
+def add_item():
+    time.sleep(1)
+    os.system("clear")
+    print("INVENTORY")
+    print("=========")
+    print()
+  
+    item = input("Enter item name: ").capitalize()
+    inventory.append(item)
+    print(f"{item} added to inventory.")
+    
+    
+def viewItem():
   time.sleep(1)
   os.system("clear")
-  name = input("Name : ")
-  topping = input("Topping : ")
-  size = input("Size : ").lower()
-  while True:
-    try:
-      qty = int(input("Quantity : "))
-      break
-    except:
-      print("ERROR: Invalid input must be a whole number")
-  cost = 0
-  if size == "s":
-    cost = 6.99
-  elif size == "m":
-    cost = 8.99
-  else:
-    cost = 10.99
-    total = cost * qty
-    total = round(total, 2)
-    row = [name, topping, size, qty, total]
-    pizza.append(row)
+  print("INVENTORY")
+  print("=========")
+  print()
+  seen = []
+  for item in inventory:
+    if item not in seen:
+      print(f"{item} {inventory.count(item)}")
+      seen.append(item)
+
+  time.sleep(2)
+def remove_item():
+    time.sleep(1)
+    os.system("clear")
+    print("INVENTORY")
+    print("=========")
+    print()
+    item = input("Enter item name: ").capitalize()
+    if item in inventory:
+        inventory.remove(item)
+        print(f"{item} removed from inventory.")
+    else:
+        print(f"{item} not found in inventory.")
 
 while True:
- time.sleep(1)
- os.system("clear")
- menu = input("1) Add: \n2) View:\n")
- if menu == "1":
-    addPizza()
- else:
-    viewPizza()
-    f = open("pizza.txt", "w")
-    f.write(str(pizza))
+    time.sleep(1)
+    os.system("clear")
+    print("INVENTORY")
+    print("=========")
+    print()
+    menu = input("1. Add item\n2. View item\n3. Remove item\n")
+    if menu == "1":
+        add_item()
+    elif menu == "2":
+        viewItem()
+    else:
+        remove_item()
+
+    f= open("inventory.txt", "w")
+    f.write(str(inventory))
     f.close()
