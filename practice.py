8169d011ae343ce835077788202ac3246b03b643
AmayaTibana <amayatibana@gmail.com>
2025-07-09 10:21:05 -0400

    day 51

--- practice.py
+++ practice.py
@@ -1,39 +1,54 @@
-"""import random, os, time
 
-mokedex = {}
 
-def prettyPrint():
-  print()
-  for keys, values in mokedex.items():
-    print(keys, end= ": ")
-    for subkey, subvalue in values.items():
-      print(subkey, subvalue, end= " | ")
+import random, os, time
+
+import os, time, random
+def add():
+  os.system("clear")
+  idea = input("Idea > ")
+  f = open("my.ideas", "a+")
+  f.write(f"{idea}\n")
+  f.close()
+  time.sleep(1)
+  os.system("clear")
+def show():
+  os.system("clear")
+  f = open("my.ideas", "r")
+  ideas = f.read().split("\n")
+  f.close()
+  ideas.remove("")
+  idea = random.choice(ideas)
+  print(idea)
+  time.sleep(2)
+  os.system("clear")
+while True:
+  menu = input("1: Add idea\n2: Show a random idea\n> ")
+  if menu == "1":
+    add()
+  else:
+    show()
+
+myEvents = {}
+
+def prettyPrint ():
+    for row in myEvents:
+      print (f"{row[0] :15^}. {row[1] : 15^}")
     print()
 
 while True:
-  name = input("Enter the beast's name: ")
-  element = input("Enter the beast's element: ")
-  move = input("Enter the beast's move: ")
-  hp = input("Enter the beast's HP: ")
-  mp = input("Enter the beast's MP: ")
-  mokedex[name] = {"element":element, "move":move, "hp":hp, "mp":mp}
-  
-  prettyPrint()
+    menu = input("1. Add Event\n2 Delete Event\n")
+    if menu == "1":
+       event = input("Enter Event: ")
+       date = input("Enter Date: ")
+       row = [date, event]
+       myEvents.append(row)
+       prettyPrint()
+    else:
+       criteria = input ("Enter event you want to delete")
+       for row in myEvents:
+           if criteria in row:
+               myEvents.remove(row)
 
-"""
-
-import time, os
-score = {}
-while True:
-  add = input("Add a new score? (y/n): > ")
-  if add == "n":
-    break
-  f = open("score.txt", "a+")
-  user_initials = input("Enter user initials: > ").upper()
-  userscore = input("Enter user score: >  ")
-  f.write(f"{user_initials} {userscore}\n")
-  f.close()
-  print("ADDED")
-  time.sleep(1)
-  os.system("clear")
-
+    f = open("events.txt","w")
+    f.write(str(myEvents))
+    f.close()
