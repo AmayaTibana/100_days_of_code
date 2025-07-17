9dd88b0af3a4f46e0137d4e6f360168150b8c622
AmayaTibana <amayatibana@gmail.com>
2025-07-17 17:05:54 -0400

    Recursion

--- practice.py
+++ practice.py
@@ -1,10 +1,27 @@
+def factorial(value):
+  if value == 1:
+    return 1
+  else:
+    return value * factorial(value-1)
+  
+print(factorial(5))
+
 def reverse(value):
   if value <= 0:
     print("Done!")
     return
-  else: 
+  else:
     for i in range(value):
-      print("😛",  end="")
+      print("👾")
+
+      def reverse(value):
+  if value <= 0:
+    print("Done!")
+    return
+  else:
+    for i in range(value):
+      print("👾", end="")
     print()
-    reverse(value - 1)
-reverse(20)
+  reverse(value - 1)
+
+reverse(10)
