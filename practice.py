828080273ceda9685e1731c796bcb492a54a473f
AmayaTibana <amayatibana@gmail.com>
2025-07-26 13:17:54 -0400

    Small practice

--- practice.py
+++ practice.py
@@ -1,25 +1,3 @@
-import pandas as pd
-
-# Sample DataFrame
-data = {'A': [1, 2, 3, 4, 5]}
-df = pd.DataFrame(data)
-
-# Calculate mean
-mean_value = df['A'].mean()
-
-# Filter
-filtered_df = df[df['A'] > mean_value]
-print(filtered_df)
-#=================================
-
-
-import pandas as pd
-import numpy as np
-
-# Sample DataFrame
-data = {'A': [1, 2, 3, 4, 5], 'B': [5, 4, 3, 2, 1]}
-df = pd.DataFrame(data)
-
-# Combine filters
-filtered_df = df[(df['A'] > 2) & (df['B'] < 4)]
-print(filtered_df)
+import coolSubroutines as cs
+cs.newPrint("red")
+print("This should be red")
