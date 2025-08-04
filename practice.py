aa2f770a06eac18b0434abc339c5c92569aa2dce
AmayaTibana <amayatibana@gmail.com>
2025-08-04 14:53:16 -0400

    Classese practice

--- practice.py
+++ practice.py
@@ -1,26 +1,47 @@
-class animal:
-  species = None
+class job:
   name = None
-  sound = None
-
-  def __init__(self, name, species, sound):
+  salary = None
+  hoursWorked = None
+  def __init__(self, name, salary, hoursWorked):
     self.name = name
-    self.species = species
-    self.sound = sound
-  def talk(self):
-    print((f"{self.name} says {self.sound}"))
-class bird(animal):
- def __init__(self, color):
-    self.name = "Bird"
-    self.species = "Avian"
-    self.sound = "Tweet"
-    self.color = color
-   
-cow = animal("Ermintrude", "Bo Taurus", "Moo")
-print(cow.sound)
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+  def print(self):
+    print("== JOB ==")
+    print()
+    print(f"{self.name:<10} {self.salary:^10} {self.hoursWorked:>10}")
+class doctor(job):
+  experience = None
+  speciality = None
+  def __init__(self, salary, hoursWorked, experience, speciality):
+    self.name = "Doctor"
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+    self.experience = experience
+    self.speciality = speciality
+  def print(self):
+    print("== JOB ==")
+    print()
+    print(f"{self.name:<10} {self.salary:^10} {self.hoursWorked:>10}")
+    print(f"{self.experience:<10} {self.speciality:>21}")
+class teacher(job):
+  subject = None
+  position = None
+  def __init__(self, salary, hoursWorked, subject,  position):
+    self.name = "Teacher"
+    self.hoursWorked = hoursWorked
+    self.salary = salary
+    self.subject = subject
+    self.position = position
 
-
-
-polly = bird("Green") 
-polly.talk()
-print(polly.color)
+  def print(self):
+    print("== JOB ==")
+    print()
+    print(f"{self.name:<10} {self.salary:^10} {self.hoursWorked:>10}")
+    print(f"{self.subject:<10} {self.position:>21}")
+lawyer = job("Lawyer", "$100,000", "40")
+lawyer.print()
+doc = doctor("$120,000", "48", "7", "Pediatric Consultant")
+doc.print()
+teach = teacher("$50,000", "48+", "CompSci", "Asst. Principal")
+teach.print()
