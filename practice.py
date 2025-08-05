fc23b401e020f5d76343ffec3407a636c3b28eba
AmayaTibana <amayatibana@gmail.com>
2025-08-05 15:53:20 -0400

    new commit

--- practice.py
+++ practice.py
@@ -1,47 +1,86 @@
-class job:
+class character:
   name = None
-  salary = None
-  hoursWorked = None
-  def __init__(self, name, salary, hoursWorked):
+  health = 100
+  mp = 100
+
+  def __init__(self, name):
     self.name = name
-    self.salary = salary
-    self.hoursWorked = hoursWorked
-  def print(self):
-    print("== JOB ==")
-    print()
-    print(f"{self.name:<10} {self.salary:^10} {self.hoursWorked:>10}")
-class doctor(job):
-  experience = None
-  speciality = None
-  def __init__(self, salary, hoursWorked, experience, speciality):
-    self.name = "Doctor"
-    self.salary = salary
-    self.hoursWorked = hoursWorked
-    self.experience = experience
-    self.speciality = speciality
-  def print(self):
-    print("== JOB ==")
-    print()
-    print(f"{self.name:<10} {self.salary:^10} {self.hoursWorked:>10}")
-    print(f"{self.experience:<10} {self.speciality:>21}")
-class teacher(job):
-  subject = None
-  position = None
-  def __init__(self, salary, hoursWorked, subject,  position):
-    self.name = "Teacher"
-    self.hoursWorked = hoursWorked
-    self.salary = salary
-    self.subject = subject
-    self.position = position
 
   def print(self):
-    print("== JOB ==")
-    print()
-    print(f"{self.name:<10} {self.salary:^10} {self.hoursWorked:>10}")
-    print(f"{self.subject:<10} {self.position:>21}")
-lawyer = job("Lawyer", "$100,000", "40")
-lawyer.print()
-doc = doctor("$120,000", "48", "7", "Pediatric Consultant")
-doc.print()
-teach = teacher("$50,000", "48+", "CompSci", "Asst. Principal")
-teach.print()
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}")
+
+  def setStats(self, health, mp):
+    self.health = health
+    self.mp = mp
+
+class player(character):
+  nickname = None
+  lives = 3
+
+  def __init__(self, nickname):
+    self.name = "Player"
+    self.nickname = nickname
+
+  def print(self):
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}\tNickname: {self.nickname}\tLives: {self.lives}")
+
+  def isAlive(self):
+    if self.lives > 0:
+      print(f"{self.nickname} lives on!")
+      return True
+    else:
+      print(f"{self.nickname} has expired!")
+      return False
+
+ian = player("Ian the mighty")
+ian.print()
+print(ian.isAlive())
+
+class enemy(character):
+  type = None
+  strength = None
+
+  def __init__(self, name, type, strength):
+    self.name = name
+    self.type = type
+    self.strength = strength
+
+  def print(self):
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}\tType: {self.type}\tStrength: {self.strength}")
+
+class orc(enemy):
+  speed = None
+
+  def __init__(self, speed):
+    self.name = "Orc"
+    self.type = "Orc"
+    self.strength = 200
+    self.speed = speed
+
+  def print(self):
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}\tType: {self.type}\tStrength: {self.strength}\tSpeed: {self.speed}")
+
+sharron = orc(250)
+gary = orc(205)
+
+sharron.print()
+gary.print()
+
+class vampire(enemy):
+  day = True
+
+  def __init__(self, day):
+    self.name = "Vampire"
+    self.type = "Vampire"
+    self.strength = 150
+    self.day = day
+
+  def print(self):
+    print(f"{self.name}\tHP: {self.health}\t MP: {self.mp}\tType: {self.type}\tStrength: {self.strength}\tDay: {self.day}")
+
+eric = vampire(False)
+eric.print()
+
+# class zombie(enemy):
+
+#new code
