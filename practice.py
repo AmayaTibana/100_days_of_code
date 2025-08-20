88b7d4a9f209c1bfb78ba06ce1e07c64cc1e0830
AmayaTibana <amayatibana@gmail.com>
2025-08-20 13:02:30 -0400

    small practice

--- practice.py
+++ practice.py
@@ -1,53 +1,15 @@
-from replit import db
-import datetime, os, time, random
+import requests, json
 
-def addEntry():
-  time.sleep(1)
-  os.system("clear")
-  timestamp = datetime.datetime.now()
-  print(f"Diary entry for {timestamp}")
-  print()
-  entry = input("> ")
-  db[timestamp] = entry
-
-def viewEntry():
-  keys = db.prefix("2")
-  for key in keys:
-    time.sleep(1)
-    os.system("clear")
-    print(f"""{key}
-    {db[key]}""")
-    print()
-    opt = input("Next or exit? > ")
-    if(opt.lower()[0]=="e"):
-      break
-
-
-
-keys = db.keys()
-if len(keys)<1:
-  print("First Run > Create account")
-  username = input("Username > ")
-  password = input("Password > ")
-  salt = random.randint(0,9999999)
-  newPassword = hash(f"{password}{salt}")
-  db[username] = {"password": newPassword, "salt": salt}
-else:
-  print("Log in")
-  username = input("Username > ")
-  password = input("Password > ")
-  if username not in keys:
-    print("Username or password incorrect")
-    exit()
-  salt = db[username]["salt"]
-  newPassword = hash(f"{password}{salt}")
-  if db[username]["password"]!=newPassword:
-    print("Username or password incorrect")
-    exit()
-while True:
-  os.system("clear")
-  menu = input("1: Add\n2: View\n> ")
-  if menu == "1":
-    addEntry()
-  else:
-    viewEntry()
+for i in range(10):
+  result = requests.get("https://randomuser.me/api/")
+  if result.status_code == 200:
+    user = result.json()
+    
+    for person in user["results"]:
+      filename = f"""{person["name"]["first"].lower()} {person["name"]["last"].lower()}.jpg"""
+      #print(filename)
+      picture = requests.get(person["picture"]["medium"])
+      f = open(filename, "wb")
+      f.write(picture.content)
+      f.close()
+      print(f"Saved {filename}") 
