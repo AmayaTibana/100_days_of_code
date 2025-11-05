dadb5f178af4b1854c5dc012053c57b928334630
AmayaTibana <amayatibana@gmail.com>
2025-11-05 14:43:55 -0500

    practice

--- practice.py
+++ practice.py
@@ -1,27 +1,30 @@
-# TODO  :clean  up  spacing   and names
-improt  os
-improt sys
+import boto3
+import psycopg2
 
-def greet( name ):
-    msg= f"Hello, {name } "
-    return  msg
+# AWS S3 setup
+s3 = boto3.client('s3')
+bucket_name = 'my-data-bucket'
+file_key = 'data/sample.csv'
 
-class user:
-    def __init__( self,first_name,last_name ):
-        self.f = first_name
-        self.l = last_name
-    def full( self ):
-        return  f"{ self.f } { self.l }"
+# Download file from S3
+s3.download_file(bucket_name, file_key, '/tmp/sample.csv')
 
-def main( ):
-    names = ["Ana","Ben" ,  "Cara" , "dan" ]
-    for n in  names :
-        print(greet( n))
-    u = user("linus","torvalds")
-    print(u.full( ))
-    # duplicate line below (intentionally bad)
-    print(u.full( ))
-    # BAD_PATH:  /home/user/docs//notes.txt
+# Connect to Redshift
+conn = psycopg2.connect(
+    dbname='mydb',
+    user='myuser',
+    password='mypassword',
+    host='redshift-cluster.endpoint.aws.com',
+    port='5439'
+)
+cur = conn.cursor()
 
-if __name__ =="__main__" :
-    main( )
+# Load data into Redshift
+with open('/tmp/sample.csv', 'r') as f:
+    cur.copy_expert("COPY my_table FROM STDIN CSV HEADER", f)
+
+conn.commit()
+cur.close()
+conn.close()
+
+print("ETL job completed successfully!")
